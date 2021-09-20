c556e06f5bdd5bdda5bdfea74b3371969edd9545
Ronnie Ayala <ronnie.ayala@outlook.com>
2021-09-20 05:06:37 -0500

    Update env constants

--- aws/env/const.go
+++ aws/env/const.go
@@ -6,8 +6,17 @@ const AwsLambdaFunctionVersion string = "AWS_LAMBDA_FUNCTION_VERSION"
 const AwsLambdaFunctionTimeout string = "AWS_LAMBDA_FUNCTION_TIMEOUT"
 
 const CevixeEventStoreTableName string = "CVX_EVENT_STORE_NAME"
+const CevixeEventStoreIndexByTime string = "CVX_EVENT_STORE_IDX_BY_TIME"
+const CevixeEventStoreIndexByDay string = "CVX_EVENT_STORE_IDX_BY_DAY"
+const CevixeEventStoreIndexByType string = "CVX_EVENT_STORE_IDX_BY_TYPE"
+const CevixeEventStoreIndexByAuthor string = "CVX_EVENT_STORE_IDX_BY_AUTHOR"
+const CevixeEventStoreIndexByTransaction string = "CVX_EVENT_STORE_IDX_BY_TRANSACTION"
+
 const CevixeControlStoreTableName string = "CVX_CONTROL_STORE_NAME"
+
 const CevixeStateStoreTableName string = "CVX_STATE_STORE_NAME"
+const CevixeStateStoreIndexByTime string = "CVX_STATE_STORE_IDX_BY_TIME"
+
 const CevixeObjectStoreBucketName string = "CVX_OBJECT_STORE_NAME"
 const CevixeEventBusTopicArn string = "CVX_EVENT_BUS_ARN"
 const CevixeGraphqlGatewayUrl string = "CVX_GRAPHQL_GATEWAY_URL"
